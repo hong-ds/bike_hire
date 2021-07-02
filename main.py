15aa30c43a8f5eb7b088adaa88a5895f23182564
Hong <hong@pop-os.localdomain>
2021-07-02 22:55:21 +0100

    update

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd
-import os
 import glob
 from functools import lru_cache
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
 
 BASE_PATH = "data"
 FILE_PATTERN = BASE_PATH + "/*/OD*"
@@ -16,16 +19,23 @@ def load_data(path_pattern: str) -> pd.DataFrame:
     df = pd.concat(filepath)
     return df
 
+def create_overview_ts(dataframe: pd.DataFrame) -> pd.DataFrame: 
+    df = dataframe.copy()
+    df.drop(["end_date", "duration_sec"], axis=1, inplace=True)
+    return df.set_index("start_date")
+
 df = load_data(FILE_PATTERN)
 df = df.loc[
-        (df.start_station_code == START_STATION) & 
-        (df.end_station_code == END_STATION) & 
         (df.start_date >= START_DATE) &
         (df.end_date < END_DATE)
     ].reset_index(drop=True)
 
+overview = create_overview_ts(df)
 
-count_by_weekday = df.groupby([df.start_date.dt.year, df.start_date.dt.weekday, df.is_member])[['start_date']].count()
+daily_traffic = overview.resample("D")["end_station_code"].count().to_frame("num_of_trips")
+sns.lineplot(data=daily_traffic)
+plt.show()
+count_by_weekday = df.groupby(["year", "weekday"])[['start_date']].count()
 count_by_weekday.columns = ["tot_counts"]
 count_by_weekday.reset_index(inplace=True)
 count_by_month = df.groupby([df.start_date.dt.year, df.start_date.dt.month, df.is_member])[['start_date']].count().reset_index(name="tot_counts")
